8091e7369d330318a830f4e7b0f09d0bcdca08c0
jovimoura <joaovictors.mouraa@gmail.com>
2026-09-15 23:18:32 -0300

    fix migration

--- csp-api/drizzle/0000_unique_rafael_vega.sql
+++ csp-api/drizzle/0000_unique_rafael_vega.sql
@@ -1,6 +1,14 @@
-CREATE TYPE "public"."task_status" AS ENUM('not_started', 'in_progress', 'paused', 'homolog', 'prod');--> statement-breakpoint
-CREATE TYPE "public"."user_profile" AS ENUM('admin', 'developer', 'agile');--> statement-breakpoint
-CREATE TABLE "tasks" (
+DO $$ BEGIN
+ CREATE TYPE "public"."task_status" AS ENUM('not_started', 'in_progress', 'paused', 'homolog', 'prod');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "public"."user_profile" AS ENUM('admin', 'developer', 'agile');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "tasks" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"title" text NOT NULL,
 	"status" "task_status" DEFAULT 'not_started' NOT NULL,
@@ -11,7 +19,7 @@ CREATE TABLE "tasks" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE "users" (
+CREATE TABLE IF NOT EXISTS "users" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" text NOT NULL,
 	"email" text NOT NULL,
@@ -21,4 +29,8 @@ CREATE TABLE "users" (
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
 --> statement-breakpoint
-ALTER TABLE "tasks" ADD CONSTRAINT "tasks_assigned_to_users_id_fk" FOREIGN KEY ("assigned_to") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
+DO $$ BEGIN
+ ALTER TABLE "tasks" ADD CONSTRAINT "tasks_assigned_to_users_id_fk" FOREIGN KEY ("assigned_to") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
